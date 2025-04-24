606e6a4aee4260209853baef69e766f4e138c645
A Akil Ganesh <akilganesh3@gmail.com>
2025-04-24 08:52:09 +0530

    Changes in package file

--- tests/Sample.spec.js
+++ tests/Sample.spec.js
@@ -4,4 +4,5 @@ test("basic test", async ({ page }) => {
   await page.goto("https://example.com");
   const title = await page.title();
   expect(title).toBe("Example Domain");
+  console.log("Successfully completed");
 });
